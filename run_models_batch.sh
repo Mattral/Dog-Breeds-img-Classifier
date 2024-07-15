965e210f2b6d60ee507fa281e2443993318cdd68
Min Htet Myet (Mattral) <88831350+Mattral@users.noreply.github.com>
2024-07-15 08:45:17 +0630

    Update run_models_batch.sh

--- run_models_batch.sh
+++ run_models_batch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # */AIPND-revision/intropyproject-classify-pet-images/run_models_batch.sh
 #                                                                             
-# PROGRAMMER: Jennifer S.
-# DATE CREATED: 02/08/2018                                  
-# REVISED DATE: 02/27/2018  - 
+# PROGRAMMER: 
+# DATE CREATED:                                  
+# REVISED DATE: - 
 # PURPOSE: Runs all three models to test which provides 'best' solution.
 #          Please note output from each run has been piped into a text file.
 #
